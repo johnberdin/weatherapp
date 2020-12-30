d3a2a170693c143fd79cf3ade3d75bcdce4d052d
johnberdin <john.berdin@stud.fh-campuswien.ac.at>
2020-12-30 23:50:14 +0100

    Initial commit

---
+++ src/main/java/team/five/Main.java
@@ -0,0 +1,23 @@
+package team.five;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("Hello, John");
+        Scene scene = new Scene(label);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
